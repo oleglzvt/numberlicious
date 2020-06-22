3b6392e65044811f0497948a3324931e2cbaca3b
Oleg Lozovatyi <lozovatyi.jr@gmail.com>
2020-06-22 16:24:04 -0400

    Updates

--- precache-manifest.038b8fcadec8a57a0bcac713962311a1.js
+++ precache-manifest.038b8fcadec8a57a0bcac713962311a1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2383b07e2694e6b82d5eb3a613321775",
+    "revision": "03f0164b8fe5aec6e66f7c55240883fb",
     "url": "/oleg-lozovatyi-project-five/index.html"
   },
   {
-    "revision": "e0942b9c60a30f17c0cd",
+    "revision": "0264fb2ec0437edd12bf",
     "url": "/oleg-lozovatyi-project-five/static/css/main.6acfc2c5.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/oleg-lozovatyi-project-five/static/js/2.a8dda089.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e0942b9c60a30f17c0cd",
-    "url": "/oleg-lozovatyi-project-five/static/js/main.aa473406.chunk.js"
+    "revision": "0264fb2ec0437edd12bf",
+    "url": "/oleg-lozovatyi-project-five/static/js/main.1274e028.chunk.js"
   },
   {
     "revision": "5ead040ef0fbfd35df1b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oleg-lozovatyi-project-five/precache-manifest.11514940af64115f2d16e6434c151457.js"
+  "/oleg-lozovatyi-project-five/precache-manifest.038b8fcadec8a57a0bcac713962311a1.js"
 );
 
 self.addEventListener('message', (event) => {
