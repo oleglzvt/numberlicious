52b98a116722d993d2f204473ae3e2c2cd32141b
Oleg Lozovatyi <lozovatyi.jr@gmail.com>
2020-06-22 11:18:16 -0400

    Updates

---
+++ precache-manifest.3b50e89c7ca60a8e3da4b7e2508b3dc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c0f609b652096a20e8d4481f4d90d6",
+    "url": "/oleg-lozovatyi-project-five/index.html"
+  },
+  {
+    "revision": "ad8df44e83889de52ea3",
+    "url": "/oleg-lozovatyi-project-five/static/css/main.6e7a9650.chunk.css"
+  },
+  {
+    "revision": "92bf7c0d03be6c78793d",
+    "url": "/oleg-lozovatyi-project-five/static/js/2.a8dda089.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oleg-lozovatyi-project-five/static/js/2.a8dda089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8df44e83889de52ea3",
+    "url": "/oleg-lozovatyi-project-five/static/js/main.dd7032c3.chunk.js"
+  },
+  {
+    "revision": "5ead040ef0fbfd35df1b",
+    "url": "/oleg-lozovatyi-project-five/static/js/runtime-main.a8967794.js"
+  }
+]);
